d253e5b021bcbac4c5897309288d4211415d6fb4
Romeo Mihalcea <eu@ciokan.ro>
2025-10-08 03:56:22 +0300

    chore: copy

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
 export const Brand = "OutreachStud.io";
-export const SiteTitle = "OutreachStud.io - Open-source outreach software for marketers";
+export const SiteTitle = "OutreachStud.io - Open-source cold emailing software for marketers";
 export const SiteDescription =
-			   "OutreachStud.io is an open-source outreach software that helps marketers keep in touch with their contacts and manage outreach campaigns at scale";
+			   "OutreachStud.io is an open-source cold emailing software that helps marketers keep in touch with their contacts and manage outreach campaigns at scale";
